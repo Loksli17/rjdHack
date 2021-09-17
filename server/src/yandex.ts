1f2bf01d8088b0ea08148f25c14e87827f9f1ae1
Alexander <sansanichick@gmail.com>
2021-09-17 23:14:46 +1000

    yandex AAAAAAAAAAAAAAAAA

--- server/src/yandex.ts
+++ server/src/yandex.ts
@@ -6,7 +6,7 @@ import { createReadStream } from "fs";
 
 const 
     FOLDER_ID = "b1g3r4lgbvftcmlle452",
-    IAM_TOKEN = "b1gtl3g1nnh8e5mjgfoq";
+    IAM_TOKEN = "t1.9euelZrMzYmUnZWbk52Rz47KnJOeju3rnpWal56Qz5KPiZSQnY-Pjp6byM_l8_cEb211-e9YA0I8_t3z90Qda3X571gDQjz-.85dRgzv0EXknkfi_TNeFR9lJVz_EahboXWFHo5rVt0U8SikrLqdybSZDhGw1jLa7IF5lb6mUrQ8zg4xrRQQdDA";
 
 const data = createReadStream("./audio/1.wav");
 
@@ -14,23 +14,38 @@ const headers = {
     "Authorization": `Bearer ${IAM_TOKEN}`
 };
 
-const params = {
-    lang: "ru-RU",
-    profanityFilter: false,
-    format: "lpcm",
-    folderId: FOLDER_ID,
+// const params = {
+//     lang: "ru-RU",
+//     profanityFilter: false,
+//     format: "lpcm",
+//     folderId: FOLDER_ID,
+//     sampleRateHertz: 8000
+// }
+const body = {
+    config: {
+        specification: {
+            languageCode: "ru-RU",
+            profanityFilter: false,
+            audioEncoding: "LINEAR16_PCM",
+            sampleRateHertz: 8000,
+        }
+    },
+    audio: {
+        uri: "AAA"
+    }
 }
 
 axios.post(
-    "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize", 
+    "https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize", 
     data, 
     {
-        headers,
-        params
+        headers
     }
 ).then((res: AxiosResponse<any>) => {
     const resData = res.data;
     console.log(resData);
+    console.log("👍");
 }).catch(err => {
-    console.error(err);
+    console.error(err.response.data);
+    console.log("error 🤔");
 });
