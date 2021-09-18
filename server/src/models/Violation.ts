563413b1de88d052f6ab882bbf736b8cf196771d
BlackLirium <mrfilin97@gmail.com>
2021-09-18 16:32:41 +1000

    typeError

--- server/src/models/Violation.ts
+++ server/src/models/Violation.ts
@@ -43,6 +43,6 @@ export default class Violation{
     public audio?: Audio | number;
 
     @ManyToOne(() => TypeError, typeError => typeError.violation)
-    public typeError?: TypeError | number;
+    public typeError?: TypeError | string;
 }
     
