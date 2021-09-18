269cfc4fe80dc3401d475b47e7c0d67a9e556116
Alexander <sansanichick@gmail.com>
2021-09-19 01:44:35 +1000

    fix

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        404
+    <div class="error-wrapper">
+        <h1>404</h1>
         <router-link to="/">назад</router-link>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .error-wrapper {
+        padding: 100px;
+    }
+</style>
